5d12051666e5974917f399273748053de2358ddc
phong0710 <phonglh071099@gmail.com>
2023-03-16 15:10:36 +0700

    Bt1

--- week1/Xử lý đồng bộ 1/test.js
+++ week1/Xử lý đồng bộ 1/test.js
@@ -1,13 +1,26 @@
-console.log("Before delay");
-
-function delayBySeconds(sec) {
-    let start = now = Date.now()
-    while(now-start < (sec*1000)) {
-        now = Date.now();
-    }
+// console.log("Before delay");
+//
+// function delayBySeconds(sec) {
+//     let start = now = Date.now()
+//     while(now-start < (sec*1000)) {
+//         now = Date.now();
+//     }
+// }
+//
+// delayBySeconds(5);
+//
+// // Executes after delay of 5 seconds
+// console.log("After delay");
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-delayBySeconds(5);
+async function demo() {
+    for (let i = 0; i < 5; i++) {
+        console.log(`Waiting ${i} seconds...`);
+        await sleep(i * 1000);
+    }
+    console.log('Done');
+}
 
-// Executes after delay of 5 seconds
-console.log("After delay");
+demo();

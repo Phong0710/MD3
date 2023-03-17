1c129d7062d244203b758e7f85289e85d0cafba2
phong0710 <phonglh071099@gmail.com>
2023-03-17 14:26:18 +0700

    th3

---
+++ week1/Xử lý bắt đồng bộ 2/TH3_AsyncThayPromse2.js
@@ -0,0 +1,19 @@
+async function onMyBrithday(isKayoSick){
+    if(!isKayoSick){
+        return 2;
+    }
+    throw new Error("I am sad")
+}
+async function doSomethingAsync(){
+    try{
+        let result = await onMyBrithday(true)
+        console.log(`I have ${result} cakes`)
+    }
+    catch (error) {
+        console.log(error)
+    }
+    finally {
+        console.log("Party")
+    }
+}
+doSomethingAsync()

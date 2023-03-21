7db8e61a4cb76e04ff5291af6d64541cae5655fc
phong0710 <phonglh071099@gmail.com>
2023-03-21 16:34:10 +0700

    TH2

--- week1/Thao tác với file/TH2_Add.js
+++ week1/Thao tác với file/TH2_Add.js
@@ -1,16 +1,33 @@
-const http=require('http')
+const http = require('http')
 const fs = require('fs')
+const qs = require("qs");
 let sever = http.createServer((req, res) => {
-    if(req.method ==="GET"){
-        fs.readFile('./TH2/index.html','utf-8',(err, data)=>{
-            res.writeHead(200,{'Content-Type':'text/html'})
+    if (req.method === "GET") {
+        fs.readFile('./TH2/index.html', 'utf-8', (err, data) => {
+            res.writeHead(200, {'Content-Type': 'text/html'})
             res.write(data);
-            return  res.end()
+            return res.end()
         })
     } else {
-        throw new Error("Error !!!!")
+        let data = '';
+        req.on('data', chunk => {
+            data += chunk
+        })
+        req.on("end", () => {
+            let name = qs.parse(data).name;
+            fs.writeFile("./TH2/data.json", name, (err) => {
+                if (err) {
+                    console.log('err')
+                }
+                return res.end('Creat success')
+
+            })
+        })
+        req.on('error', () => {
+            console.log('Error')
+        })
     }
 })
-sever.listen(8080,'localhost',()=>{
+sever.listen(8080, 'localhost', () => {
     console.log('sever is running')
 })
